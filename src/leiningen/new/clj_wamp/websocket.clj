9d0b0ab93696f00fb926cad857dec633b507e3eb
Christopher Martin <cgmartin@gmail.com>
2013-06-27 22:47:49 -0400

    Update for clj-wamp 1.0.0-beta2

--- src/leiningen/new/clj_wamp/websocket.clj
+++ src/leiningen/new/clj_wamp/websocket.clj
@@ -1,4 +1,5 @@
 (ns {{sanitized-ns}}.websocket
+  (:use [{{sanitized-ns}}.config :only [conf]])
   (:require [clojure.tools.logging :as log]
             [org.httpkit.server :as http-kit]
             [clj-wamp.server :as wamp]))
@@ -29,17 +30,15 @@
 (defn wamp-handler
   "Returns a http-kit websocket handler with wamp subprotocol"
   [req]
-  (http-kit/with-channel req channel
-    (if-not (http-kit/websocket? channel)
-      (http-kit/close channel)
-      (wamp/http-kit-handler channel
-        {:on-open        on-open
-         :on-close       on-close
-         :on-call        {(rpc-url "echo")  identity
-                          (rpc-url "throw") (fn [] (throw (Exception. "An exception")))
-                          :on-before        on-before-call}
-         :on-subscribe   {(evt-url "chat")  true}
-         :on-publish     {(evt-url "chat")  true
-                          :on-after         on-publish}}))))
+  (wamp/with-channel-validation req channel (:ws-origins-re (conf))
+    (wamp/http-kit-handler channel
+      {:on-open        on-open
+       :on-close       on-close
+       :on-call        {(rpc-url "echo")  identity
+                        (rpc-url "throw") (fn [] (throw (Exception. "An exception")))
+                        :on-before        on-before-call}
+       :on-subscribe   {(evt-url "chat")  true}
+       :on-publish     {(evt-url "chat")  true
+                        :on-after         on-publish}})))
 
 
