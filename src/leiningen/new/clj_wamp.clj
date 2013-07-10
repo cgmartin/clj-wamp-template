6e36bfe1154e1cafea8e51474e38505a2dae4dec
Christopher Martin <cgmartin@gmail.com>
2013-07-10 11:37:20 -0400

    Fix project filesystem/namespace path for names with dashes

--- src/leiningen/new/clj_wamp.clj
+++ src/leiningen/new/clj_wamp.clj
@@ -11,19 +11,20 @@
 (defn clj-wamp
   "FIXME: write documentation"
   [name]
-  (let [fs-path (name-to-path name)
+  (let [fs-path        (name-to-path name)
+        sanitized-name (sanitize-ns name)
         data {:name         name
               :year         (year)
               :fs-path      fs-path
-              :sanitized-ns (sanitize-ns name)}]
+              :sanitized-ns sanitized-name}]
     (->files data
       ["README.md"                       (render "README.md" data)]
       ["project.clj"                     (render "project.clj" data)]
-      ["src/{{sanitized-ns}}/websocket.clj" (render "websocket.clj" data)]
-      ["src/{{sanitized-ns}}/routes.clj"    (render "routes.clj" data)]
-      ["src/{{sanitized-ns}}/main.clj"      (render "main.clj" data)]
-      ["src/{{sanitized-ns}}/config.clj"    (render "config.clj" data)]
-      ["resources/public/index.html"        (render "public/index.html" data)]
-      ["resources-dev/config.clj"           (binary "resources_dev/config.clj")]
-      ["resources-dev/log4j.properties"     (binary "resources_dev/log4j.properties")])
-    (println "Created new clj-wamp project:" fs-path)))
+      ["src/{{fs-path}}/websocket.clj"   (render "websocket.clj" data)]
+      ["src/{{fs-path}}/routes.clj"      (render "routes.clj" data)]
+      ["src/{{fs-path}}/main.clj"        (render "main.clj" data)]
+      ["src/{{fs-path}}/config.clj"      (render "config.clj" data)]
+      ["resources/public/index.html"     (render "public/index.html" data)]
+      ["resources-dev/config.clj"        (binary "resources_dev/config.clj")]
+      ["resources-dev/log4j.properties"  (binary "resources_dev/log4j.properties")])
+    (println "Created new clj-wamp project:" sanitized-name)))
